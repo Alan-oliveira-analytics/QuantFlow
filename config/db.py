360ae3a3a9fa370f4012233ea437fbc4f9c2d3f3
Alan Oliveira <alanoliveira@MacBook-Air-de-Alan.local>
2026-06-30 10:58:21 -0300

    preparacao estrutural para deploy

--- config/db.py
+++ config/db.py
@@ -16,15 +16,21 @@ def get_engine():
 
     load_dotenv(ENV_PATH)
 
-    user = os.getenv('POSTGRES_USER')
-    password = os.getenv('POSTGRES_PASSWORD')
-    database = os.getenv('POSTGRES_DB')
-    host = 'localhost'
+    url = os.getenv('DATABASE_URL')
 
+    if url:
+        # produção
+        url = url.replace('postgresql://', 'postgresql+psycopg2://', 1)
+
+    else:
+        # local/desenvolvimento
+        user = os.getenv('POSTGRES_USER')
+        password = os.getenv('POSTGRES_PASSWORD')
+        database = os.getenv('POSTGRES_DB')
+        host = 'localhost'
+        url = f'postgresql+psycopg2://{user}:{quote_plus(password)}@{host}:5433/{database}'
 
 
     logger.info('Creating database engine...')
 
-    return create_engine(
-        f'postgresql+psycopg2://{user}:{quote_plus(password)}@{host}:5433/{database}'
-    )
+    return create_engine(url)
